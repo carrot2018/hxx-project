bde221ed6aaeb8c2d048211cd12e043cb8a85549
carrot2018 <1174955966@qq.com>
2018-11-27 15:51:12 +0800

    '完成了数据库的连接'

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+
+router.get('/', function (req, res, next) {
+  res.render('login');
+});
+
+router.get('/register.html', function (req, res) {
+  res.render('register');
+});
+
+router.get('/index.html', function (req, res) {
   res.render('index', { title: 'Express' });
+});
+
+// router.get('/users.html', function (req, res) {
+//   res.render('users');
+// });
+
+router.get('/brand.html', function (req, res) {
+  res.render('brand');
+});
+
+router.get('/phone.html', function (req, res) {
+  res.render('phone');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient
 var router = express.Router();
 
+var url = 'mongodb://127.0.0.1:27017'
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', function (req, res, next) {
 
+  MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
+
+    if (err) {
+      console.log('链接数据库失败', err);
+      res.render('error', {
+        message: '链接数据库失败',
+        error: err
+      });
+      return;
+    }
+    var db = client.db('project');
+    db.collection('user').find().toArray(function (err, data) {
+      if (err) {
+        console.log('查询用户失败', err);
+        res.render('error', {
+          message: '查询失败',
+          error: err
+        })
+      } else {
+        console.log(data);
+        res.render('users', {
+          list: data
+        });
+      }
+      client.close();
+    });
+  });
+});
 module.exports = router;
